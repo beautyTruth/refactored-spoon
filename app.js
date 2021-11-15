018c8c9f33095d6794c98f239d0c87ea6bc4a807
Phil <happy.melodist@gmail.com>
2021-11-14 18:12:31 -0600

    complete progress bar lecture noting that it's just a set timeout function

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+// const progress = document.querySelector(".progress-done");
+
+// setTimeout(function () {
+//   progress.style.width = progress.getAttribute("data-done") + "%";
+//   progress.style.opacity = 1;
+// }, 500);
+
 const progress = document.querySelector(".progress-done");
 
-setTimeout(function () {
+setTimeout(() => {
   progress.style.width = progress.getAttribute("data-done") + "%";
   progress.style.opacity = 1;
 }, 500);
+
+// this is just for show -- the dynamic movement isn't tied to data loaded
